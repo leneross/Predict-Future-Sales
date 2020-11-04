233de34098fd71a17a5940406f6297199ea178ca
Lene <Lene.rossland@gmail.com>
2020-11-04 10:37:55 +0100

    translatorfinished

---
+++ translator.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+from googletrans import Translator
+import httpx
+
+#Import items
+items_russia = pd.read_csv('items.csv')
+
+#%%
+#Translate itemname
+trans = Translator()
+items = []
+for item in items_russia['item_name']:
+    timeout = httpx.Timeout(5)
+    translate = trans.translate(item,dest='en', timeout=timeout)
+    items.append(translate.text)
+
+#Create a new CSV with translated item-names. (complicated way)
+items_to_df = pd.DataFrame(items, columns=['item_Name'])
+not_trans = items_russia[['item_id', 'item_category_id']]    
+frame = [not_trans, items_to_df]
+df_concat = pd.concat(frame, axis=1, join='inner')
+df_concat.to_csv('items_translated.csv')
+
+#%%
+# Import shops
+shops_russia = pd.read_csv('shops.csv')
+
+shops = []
+for items in shops_russia['shop_name']:
+    timeout = httpx.Timeout(5)
+    translate = trans.translate(items,dest='en', timeout=timeout)
+    shops.append(translate.text)
+    
+#Create new CSV with translated shop_names (easy way)
+shops_to_df = pd.DataFrame(shops, columns=['shop_Name'])
+shops_russia['shop_Name'] = shops_to_df
+del shops_russia['shop_name']
+shops_russia.to_csv('shops_translated.csv')
